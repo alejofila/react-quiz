d50d074de4ed8c0819ac6f4bef279e06eb6ca283
Jose Agudelo <jose.agudelo@edreamsodigeo.com>
2023-04-18 20:01:09 +0200

    Updating package.json againx2

--- src/questions.js
+++ src/questions.js
@@ -2,12 +2,12 @@ const allQuestions = [
     {
         "question" : "¿De que color era mi camiseta el dia en que nos conocimos?",
         "correct_answer" : "Negra",
-        "incorrect_answers" : ["Verde feid","Azul","Blanca"]
+        "incorrect_answers" : ["Verde feid","Azul","Blanca", "Roja"]
     },
     {
-        "question" : "Ratatotuile",
-        "correct_answer" : "blabla",
-        "incorrect_answers" : ["A","B","C"]
+        "question" : "¿Sabes que tipo de aniversario estamos cumpliendo hoy?",
+        "correct_answer" : "Bodas de algodón",
+        "incorrect_answers" : ["Bodas de seda","Bodas de cobre","Bodas de cuero", "Bodas de arcilla"]
     },{
         "question" : "Ratatotuile",
         "correct_answer" : "blabla",
